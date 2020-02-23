48850e951d76b41a6265e4de36a4e7f5dfa13aed
AwesomeMP8 <ihascrudmail@gmail.com>
2020-02-23 11:27:36 -0800

    Started "Exploring Generics & Iterators"

---
+++ src/hw5/exploringgenericsanditerators/ExploringIterators.java
@@ -0,0 +1,14 @@
+package hw5.exploringgenericsanditerators;
+
+import java.util.*;
+
+public class ExploringIterators {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		GrabBag<String> things = new GrabBag<>();
+
+	}
+
+	public static void populate(GrabBag<String> bag, int num, Random r) {
+	}
+}
