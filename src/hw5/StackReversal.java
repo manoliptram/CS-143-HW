82e4525e302f695e87d01d7d1c096869302a56bf
unknown <ihascrudmail@gmail.com>
2020-02-28 10:47:09 -0800

    Stack Reversal Sample Code Added

--- src/hw5/StackReversal.java
+++ src/hw5/StackReversal.java
@@ -10,13 +10,16 @@ public class StackReversal {
 		
 		s.add(3);
 		s.add(5);
-		
+
 		insertAtBottom(4, s);
-		
+
 		System.out.println(s);
+
+		s = reverse(s);
+
+		System.out.println("Reversed: " + s);
 	}
 
-	
 	public static void insertAtBottom(int val, Stack<Integer> s)
 	{
 		if (s.isEmpty())
@@ -24,9 +27,21 @@ public class StackReversal {
 			s.add(val);
 			return;
 		}
-		
+
 		int temp = s.pop();
 		insertAtBottom(val, s);
 		s.add(temp);
+	}
+
+	private static Stack<Integer> reverse(Stack<Integer> s)
+	{
+		if (s.size() == 0)
+		{
+			return new Stack<Integer>();
+		}
+		int temp = s.pop();
+		Stack<Integer> ret = reverse(s);
+		ret.add(0, temp);
+		return ret;
 	}
 }
